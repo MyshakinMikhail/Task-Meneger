458658cbedf9f3fed8654608fbd1e275ba915c89
Mikhail Myshakin <myshakinm@mail.ru>
2025-05-11 15:37:29 +0300

    added main handlers

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,14 @@
+# backend/app/main.py
 from contextlib import asynccontextmanager
 from fastapi.exceptions import RequestValidationError
 from fastapi import FastAPI, HTTPException, Request
-from .routers import auth
-from .database import engine
-from .models.users import Base, User
-import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
+from .routers import auth, note  # Импортируем новый роутер note
+from .database import engine, Base
+
+# from .models.users import User
+# from .models.notes import Note
+# import uvicorn
 
 
 @asynccontextmanager
@@ -16,14 +20,30 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173"
+    ],  # http://localhost:5173, * - для разработки
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(auth.router, prefix="/auth", tags=["Аутентификация"])
+app.include_router(note.router)  # Подключаем роутер note
+
+
 @app.get("/", tags=["Главная"])
 async def root():
     return {"message": "FastAPI auth app is running!"}
 
-app.include_router(auth.router, prefix="/auth", tags=["Аутентификация"])
 
 @app.exception_handler(RequestValidationError)
 async def handle_validation_error(request: Request, exc: RequestValidationError):
     error_message = exc.errors()[0]["msg"][13:]
     raise HTTPException(status_code=422, detail=error_message)
 
+
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="localhost", port=8000, reload=True)
