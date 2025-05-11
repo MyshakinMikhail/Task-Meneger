b221a4b8f73059958ac0e8c9730e0adbb1710e04
Mikhail Myshakin <myshakinm@mail.ru>
2025-05-11 18:40:38 +0300

    the handle get_description by Gigachat is working

--- backend/app/routers/gigachat.py
+++ backend/app/routers/gigachat.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 
 from gigachat import GigaChat
 
-from backend.app.config import GIGACHAT_AUTH_TOKEN
+from ..config import GIGACHAT_AUTH_TOKEN
 
 router = APIRouter()
 
@@ -10,14 +10,17 @@ model = GigaChat(
     credentials=GIGACHAT_AUTH_TOKEN,
     scope="GIGACHAT_API_PERS",
     model="GigaChat",
-    ca_bundle_file="backend/cert/russian_trusted_root_ca.cer",
+    verify_ssl_certs=True,
+    ca_bundle_file="cert/russian_trusted_root_ca.cer",
 )
 
 
 @router.get("/get_description")
-async def generate_description(name: str):
-    response = model.chat(f"Напиши краткое и простое описание задачи: {name}. Описание должно быть понятным, без сложных терминов и длинных предложений.")
+async def generate_description(title: str):
+    response = model.chat(
+        f"Напиши краткое и простое описание задачи: {title}. Описание должно быть понятным, без сложных терминов и длинных предложений."
+    )
     return {
         "message": "Description is ready",
-        "response": f"{response.choices[0].message.content}"
+        "description": f"{response.choices[0].message.content}",
     }
