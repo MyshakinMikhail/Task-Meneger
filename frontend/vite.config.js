177c86080d523a2e5d3f059f85ed378b0a27e8db
Mikhail Myshakin <myshakinm@mail.ru>
2025-05-16 22:28:22 +0300

    formatted

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,14 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
+const VITE_API_URL = "http://backend:8000/";
+
 export default defineConfig({
     plugins: [react()],
     server: {
         proxy: {
             "/api": {
-                target: "http://localhost:8000/",
+                target: `${VITE_API_URL}`,
                 changeOrigin: true,
                 rewrite: (path) => path.replace(/^\/api/, ""),
             },
