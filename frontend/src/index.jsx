b30a5b118cad5fa4ba0fb01bec9ed7fd1e1ba607
Mikhail Myshakin <myshakinm@mail.ru>
2025-05-17 19:52:39 +0300

    rewrite import path

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import { TasksProvider } from "./components/contexts/TaskContext.jsx";
+import { TasksProvider } from "./components/contexts/TasksContext.jsx";
 import "./styles/index.css";
 
 createRoot(document.getElementById("root")).render(
