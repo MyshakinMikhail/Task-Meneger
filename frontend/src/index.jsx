7dcf3f5e8276ef4e01c5da792dcdb762d28fc1e0
Mikhail Myshakin <myshakinm@mail.ru>
2025-05-17 15:44:22 +0300

    added TasksProvider

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,9 +1,10 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import { TasksProvider } from "./components/contexts/TaskContext.jsx";
 import "./styles/index.css";
 
 createRoot(document.getElementById("root")).render(
-    // <TasksProvider>
-    <App />
-    // </TasksProvider>
+    <TasksProvider>
+        <App />
+    </TasksProvider>
 );
