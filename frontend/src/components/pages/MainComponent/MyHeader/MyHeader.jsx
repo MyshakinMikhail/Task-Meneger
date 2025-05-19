f45da79cb6d1c4e4ff183f1cdf591e4be11c13ed
Mikhail Myshakin <myshakinm@mail.ru>
2025-05-19 19:33:40 +0300

    added component Logout

--- frontend/src/components/pages/MainComponent/MyHeader/MyHeader.jsx
+++ frontend/src/components/pages/MainComponent/MyHeader/MyHeader.jsx
@@ -1,49 +1,24 @@
 import { UserOutlined } from "@ant-design/icons";
 import { Avatar, Dropdown, Layout } from "antd";
 import { useNavigate } from "react-router-dom";
-import useAuthStore from "../../../../hooks/useAuthStore";
-import api from "./../../../utils/api";
+import Logout from "./../../../API/Logout";
 import classes from "./MyHeader.module.css";
 
 const { Header } = Layout;
 
 export default function MyHeader({ username }) {
-    const navigate = useNavigate();
-
     const userMenuItems = [{ key: "logout", label: "Выйти" }];
+    const navigate = useNavigate();
 
     const user = {
         name: username,
         avatar: null,
-        email: "john.doe@example.com",
     };
 
     async function handleMenuClick({ key }) {
         switch (key) {
-            case "profile":
-                console.log("Click on profile");
-                break;
-            case "settings":
-                console.log("Click on settings");
-                break;
             case "logout":
-                try {
-                    const response = await api.post("/auth/logout");
-                    console.log("Ответ с бэка:", response.data.message);
-
-                    useAuthStore.getState().logout();
-                    console.log("Вы вышли из системы");
-                    console.log(
-                        "Текущее состояние accessToken - " +
-                            localStorage.getItem("access")
-                    );
-                    navigate("/login");
-                } catch (error) {
-                    console.error(
-                        "Ошибка при выходе:",
-                        error.response.data.detail
-                    );
-                }
+                Logout(navigate);
                 break;
             default:
                 break;
@@ -53,27 +28,14 @@ export default function MyHeader({ username }) {
     return (
         <Header className={classes.header}>
             <div className={classes.logo}>TaskHive</div>
-            <div
-                style={{
-                    display: "flex",
-                    alignItems: "center",
-                    gap: "16px",
-                }}
-            >
+            <div className={classes.main}>
                 <Dropdown
                     menu={{ items: userMenuItems, onClick: handleMenuClick }}
                     placement="bottomRight"
                 >
-                    <div
-                        style={{
-                            cursor: "pointer",
-                            display: "flex",
-                            alignItems: "center",
-                            gap: "8px",
-                        }}
-                    >
+                    <div className={classes.avatar}>
                         <Avatar icon={<UserOutlined />} />
-                        <span style={{ color: "white" }}>{user.name}</span>
+                        <span className={classes.span}>{user.name}</span>
                     </div>
                 </Dropdown>
             </div>
